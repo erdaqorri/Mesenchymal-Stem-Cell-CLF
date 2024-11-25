4bf20cf5bf2d784151a90366b9f437dc805f34a9
Erda Alexandria Qorri <97244940+erdaqorri@users.noreply.github.com>
2024-11-25 14:54:44 +0100

    Update umap_clustering_by_sample.R

--- Single_Cell_RNA_Seq/Figure_Scripts/umap_clustering_by_sample.R
+++ Single_Cell_RNA_Seq/Figure_Scripts/umap_clustering_by_sample.R
@@ -6,7 +6,7 @@ library(patchwork)
 # Run UMAP on the filtered and scaled seurat object
 seurat_obj_filtered_scaled <- RunUMAP(seurat_obj_filtered_scaled, dims = 1:10)
 
-# Generate the clustering
+# Visualize the clusters
 DimPlot(seurat_obj_filtered_scaled, reduction = "umap", group.by = "sample",
                        pt.size = 0.5,
                        cols = c("#0d1b53", "#fe828c"))
