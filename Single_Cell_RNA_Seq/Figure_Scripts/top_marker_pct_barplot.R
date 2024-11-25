7e35011c64565b5a27fb1d077ab7268dbb6143b4
Erda Alexandria Qorri <97244940+erdaqorri@users.noreply.github.com>
2024-11-25 14:39:01 +0100

    add top marker percentages barplot

---
+++ Single_Cell_RNA_Seq/Figure_Scripts/top_marker_pct_barplot.R
@@ -0,0 +1,33 @@
+# Figure X: Analysis of top marker genes in late-passage cell populations and characterization of CRYAB interacting partners
+# Barplot
+
+# Input data: supplementary table 10
+
+supplementary_table_10_df <- read.csv("/path/Supplementary Table 10.csv")
+supplementary_table_10_df <- read.csv("~/Downloads/Supplementary Table 10.csv")
+
+# Define cluster order based on their Passage status
+subcluster_order <- c(0, 3, 4, 7, 1, 2, 5, 6)
+
+# Define colors with the same palette as the other plots
+ridge_colors <- c(
+  "#bfadcc", "#ffe1f5", "#dfbbda", "#896790", 
+  "#008080", "#006666", "#005959", "#004c4c"
+)
+
+
+supplementary_table_10_df %>%
+  mutate(Clusters = factor(Clusters, levels = subcluster_order)) %>%
+  ggplot(aes(x = Clusters, y = percentage, fill = Clusters)) +
+  geom_bar(stat = "identity") + 
+  geom_text(aes(label = sprintf("%.1f", percentage)), vjust = -0.5) +
+  # geom_text(aes(label = sprintf("%.1f%%", percentage)), vjust = -0.5) +
+  labs(x = "Clusters", y = "Percentage (%)") +
+  theme_classic() +
+  theme(
+    axis.text.x = element_text(size = 14),
+    axis.text.y = element_text(size = 14),
+    axis.title.x = element_text(size = 16),
+    axis.title.y = element_text(size = 16)
+  ) +
+  scale_fill_manual(values = ridge_colors)
