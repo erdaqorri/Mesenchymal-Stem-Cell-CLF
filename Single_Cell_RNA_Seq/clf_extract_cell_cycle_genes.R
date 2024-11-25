ca549f390cfcfcc89a517d4f4d2d3fa564981001
Erda Alexandria Qorri <97244940+erdaqorri@users.noreply.github.com>
2024-11-25 15:48:23 +0100

    Update clf_extract_cell_cycle_genes.R

--- Single_Cell_RNA_Seq/clf_extract_cell_cycle_genes.R
+++ Single_Cell_RNA_Seq/clf_extract_cell_cycle_genes.R
@@ -1,26 +1,25 @@
 # Title: genes2clfamiliaris
-# Based on the script from Victor Barrera
+# Based on the script from tiny atlas (https://github.com/hbc/tinyatlas/tree/master)
 
-# Loading libraries
 library(biomaRt)
 library(tidyverse)
 library(RCurl)
 library(dplyr)
 library(rio)
 
-## Download latest version of the human cell cycle genes
+# Download latest version of the human cell cycle genes
 human_cell_cycle_genes <- getURL("https://raw.githubusercontent.com/hbc/tinyatlas/master/cell_cycle/Homo_sapiens.csv")
 human_cell_cycle_genes <- read.csv(text = human_cell_cycle_genes)
 
-## Download Ensembl data for hsapiens
+# Download Ensembl data for hsapiens
 ensembl <- useMart("ensembl") # creates an object of the mart class
-ensembl.human <- useMart("ensembl", dataset = "hsapiens_gene_ensembl") # probably extracts all the human genes
+ensembl.human <- useMart("ensembl", dataset = "hsapiens_gene_ensembl")
 
-## Download Ensembl data for clfamiliaris
+# Download Ensembl data for clfamiliaris
 ensembl <- useMart("ensembl")
 ensembl.clf <- useMart("ensembl", dataset = "clfamiliaris_gene_ensembl")
 
-# Obtaining orthologue
+# Obtaining orthologue information
 clf_ortholog_information <-
   getBM(
     attributes = c(
@@ -34,16 +33,10 @@ clf_ortholog_information <-
     mart = ensembl.human
   )
 
-#clf_ortholog_information %>%
- #View()
-
 # We only keep the ones with one2one orthology type and high orthology confidence
 clf_selected_genes <-
   doggo_ortholog_information %>% dplyr::filter(clfamiliaris_homolog_orthology_type == "ortholog_one2one",
                                   clfamiliaris_homolog_orthology_confidence == 1)
-
-#clf_selected_genes %>%
-  #View()
 
 ## Adding Cell Cycle information
 clf_cc_genes <- clf_selected_genes %>% dplyr::inner_join(human_cc_genes, by = c("ensembl_gene_id" = "geneID")) %>% 
